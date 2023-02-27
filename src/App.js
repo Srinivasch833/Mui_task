646e196ae0b3b4867dcd8e13a7993ec5afac04a9
Srinivasch833 <srinivasch833@gmail.com>
2023-02-27 14:39:54 +0530

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './Components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <NavigationBar/>
     </div>
   );
 }
